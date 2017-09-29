5f7dd5992f772ffeaedc58cfb8212429aed4f061
TanvirArjel <tanvirahmad.du@gmail.com>
2017-09-29 09:00:13 +0600

    Dependency Injection Using Spring.Net Removed

--- Using Spring.NET/ContosoUniversity/ContosoUniversity/Scripts/Script.js
+++
@@ -1,9 +0,0 @@
-﻿$(function () {
-    $(".date-picker").datepicker(
-    {
-        changeMonth : true,
-        changeYear: true,
-        yearRange: "-100:+0",
-        dateFormat: "dd/mm/yy"
-    });
-});
